b784088c3279ba1b42fa82a3313f0c1977e4a657
shivkumarkanojiya <132676192+shivkumarkanojiya@users.noreply.github.com>
2023-08-31 18:57:36 +0530

    Initial commit
    This API Use in project:DotCoreCRUDAppConsumeAPI

---
+++ ASPNETCoreWebAPICRUDUsingEF/Controllers/StudentAPIController.cs
@@ -0,0 +1,78 @@
+﻿using ASPNETCoreWebAPICRUDUsingEF.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace ASPNETCoreWebAPICRUDUsingEF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly AspDotNetCoreDBContext context;
+
+        public StudentAPIController(AspDotNetCoreDBContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudent()
+        {
+            var data = await context.Students.ToListAsync();
+            return Ok(data);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+            var data = await context.Students.FindAsync(id);
+
+            if (data == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+
+                return Ok(data);
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(Student std)
+        {
+            await context.Students.AddAsync(std);
+            await context.SaveChangesAsync();
+            return Ok(std);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Student>> UpdateSudent(int id,Student std)
+        {
+            if(id !=std.Id)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                context.Entry(std).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return Ok(std);
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> DeleteSudent(int id)
+        {
+            var std=await context.Students.FindAsync(id);    
+            if(std == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                context.Students.Remove(std);
+                await context.SaveChangesAsync();
+
+                return Ok(std);
+            }
+        }
+    }
+}
---
+++ ASPNETCoreWebAPICRUDUsingEF/Models/AspDotNetCoreDBContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ASPNETCoreWebAPICRUDUsingEF.Models
+{
+    public partial class AspDotNetCoreDBContext : DbContext
+    {
+        public AspDotNetCoreDBContext()
+        {
+        }
+
+        public AspDotNetCoreDBContext(DbContextOptions<AspDotNetCoreDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.FatherName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StudentGender)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StudentName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
